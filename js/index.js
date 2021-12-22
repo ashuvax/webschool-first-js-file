6cf71d8bdc7c0bc78497b88356a74a60ddfc82cc
ashuvax <ash6783397@gmail.com>
2021-12-22 20:10:26 +0200

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,2 +1,11 @@
-let webschool = "ss";
-console.log(webschool);
+// let webschool = "ss";
+// console.log(webschool);
+// alert(webschool);
+// prompt("הכנס שם");
+
+const num1 = prompt("enter number 1");
+const num2 = prompt("enter number 2");
+const num = Math.round(parseInt(num1) / parseInt(num2));
+const total =
+  "The result of the division between " + num1 + " and " + num2 + " is " + num;
+alert(total);
